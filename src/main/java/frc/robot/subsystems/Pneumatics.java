d056ae7b9ff2ab7ebadea4813d932a213c740029
amathew4538 <121983385+amathew4538@users.noreply.github.com>
2026-01-14 17:14:43 -0600

    Update Pneumatics.java

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,28 +1,28 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.Compressor;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Pneumatics extends SubsystemBase{
-    private final Compressor m_compressor = new Compressor(5, PneumaticsModuleType.REVPH);
-    private final DoubleSolenoid m_doubleSolenoidLeft = new DoubleSolenoid(5, PneumaticsModuleType.REVPH, 0, 1);
-    private final DoubleSolenoid m_doubleSolenoidRight = new DoubleSolenoid(5, PneumaticsModuleType.REVPH, 14, 15);
+public class Pneumatics extends SubsystemBase {
+    private final PneumaticHub m_pneumaticHub = new PneumaticHub(5);
 
-    public Pneumatics(){
-        m_compressor.enableDigital();
+    private final DoubleSolenoid m_solenoidLeft = m_pneumaticHub.makeDoubleSolenoid(0, 1);
+    private final DoubleSolenoid m_solenoidRight = m_pneumaticHub.makeDoubleSolenoid(14, 15);
 
-        m_doubleSolenoidLeft.set(kReverse);
-        m_doubleSolenoidRight.set(kReverse);
-    };
+    public Pneumatics() {
+        m_pneumaticHub.enableCompressorDigital();
+
+        m_solenoidLeft.set(kReverse);
+        m_solenoidRight.set(kReverse);
+    }
 
     public Command toggleSolenoids() {
         return this.runOnce(() -> {
-            m_doubleSolenoidLeft.toggle();
-            m_doubleSolenoidRight.toggle();
+            m_solenoidLeft.toggle();
+            m_solenoidRight.toggle();
         });
     }
 }
