7b2209ae2dbd3de11a9c59ac757dfa5ec6b31a7b
amathew4538 <amathew4538@colliervilledragons.org>
2026-02-10 15:18:56 -0600

    Created Elastic config and defined Gyroscope

--- src/main/java/frc/robot/subsystems/Gyrosope.java
+++ src/main/java/frc/robot/subsystems/Gyrosope.java
@@ -1,7 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gyrosope extends SubsystemBase{
-    
+    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 }
--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,30 +1,44 @@
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class Pneumatics extends SubsystemBase {
     private final PneumaticHub m_pneumaticHub = new PneumaticHub(5);
-    
+
     private final DoubleSolenoid m_solenoidLeft = m_pneumaticHub.makeDoubleSolenoid(0, 1);
     private final DoubleSolenoid m_solenoidRight = m_pneumaticHub.makeDoubleSolenoid(14, 15);
 
+    private final ShuffleboardTab m_tab = Shuffleboard.getTab("Pneumatics");
+    
+    private final GenericEntry m_compressorEntry = m_tab.add("Compressor On", false)
+        .withWidget(BuiltInWidgets.kBooleanBox)
+        .getEntry();
+
+    private final GenericEntry m_pressureFullEntry = m_tab.add("Pressure Full", false)
+        .withWidget(BuiltInWidgets.kBooleanBox)
+        .getEntry();
+
     public Pneumatics() {
         m_pneumaticHub.enableCompressorDigital();
-        
+
         m_solenoidLeft.set(kReverse);
         m_solenoidRight.set(kReverse);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putBoolean("Compressor On", m_pneumaticHub.getCompressor());
-        SmartDashboard.putBoolean("Pressure Switch", m_pneumaticHub.getPressureSwitch());
+        m_compressorEntry.setBoolean(m_pneumaticHub.getCompressor());
+        m_pressureFullEntry.setBoolean(!m_pneumaticHub.getPressureSwitch());
     }
 
     public Command toggleSolenoids() {
