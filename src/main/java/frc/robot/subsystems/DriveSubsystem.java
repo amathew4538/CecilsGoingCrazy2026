7db625552af9303a11cf502d4f2a6afa967fb8c2
amathew4538 <amathew4538@colliervilledragons.org>
2026-02-11 15:03:57 -0600

    Try to fix drive widget

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -10,6 +10,7 @@ import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig; //C: this is mass importation
+
 public class DriveSubsystem extends SubsystemBase {
   private final SparkMax m_leftLeader = new SparkMax(4, MotorType.kBrushless); //C: defines sparkmax 4 as front left, as a leader to let us know it's meant to receive input
   private final SparkMax m_leftFollower = new SparkMax(3, MotorType.kBrushless); //C: defines sparkmax 3 as back left, as a follower to let us know it matches its leaders' input
@@ -20,6 +21,7 @@ public class DriveSubsystem extends SubsystemBase {
 
   public DriveSubsystem() {
     SparkMaxConfig leftConfig = new SparkMaxConfig(); //C: preparing to pair the left side
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
     SparkMaxConfig rightConfig = new SparkMaxConfig(); //C: ditto for right
 
     // Configure followers to follow their respective leaders
@@ -28,11 +30,12 @@ public class DriveSubsystem extends SubsystemBase {
 
     // Invert one side so positive power moves the robot forward
     rightConfig.inverted(true); //C: not much i can explain here when the above comment said it all already
+    rightLeaderConfig.inverted(true);
 
     // 3. Apply configurations
     m_leftLeader.configure(new SparkMaxConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     m_leftFollower.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_rightLeader.configure(new SparkMaxConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     m_rightFollower.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     ShuffleboardTab m_tab = Shuffleboard.getTab("Drive System");
