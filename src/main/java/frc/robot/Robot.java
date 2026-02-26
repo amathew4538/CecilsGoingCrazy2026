1092ce1541d6c7708a475eee954926c8b86cb5a1
amathew4538 <amathew4538@colliervilledragons.org>
2026-02-25 18:47:16 -0600

    get Better Comments, try to fix drive (no workie)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,6 +28,9 @@ public class Robot extends LoggedRobot {
    * initialization code.
    */
   public Robot() {
+    // ! Driving seems to be broken with this. Confirmed: Sim on Mac with PS4
+
+    m_robotContainer = new RobotContainer();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     Logger.recordMetadata("ProjectName", "CecilsGoingCrazy2026"); // Set a metadata value
@@ -42,9 +45,7 @@ public class Robot extends LoggedRobot {
         Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
     }
     Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
-    m_robotContainer = new RobotContainer();
   }
 
   /**
